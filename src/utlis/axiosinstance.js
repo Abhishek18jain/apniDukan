aefcd0b98ea1a0860da024bc6fc948d590fe011f
jainshab1803 <abhijain1803g@gmail.com>
2025-12-01 02:58:00 +0530

    done

--- src/utlis/axiosinstance.js
+++ src/utlis/axiosinstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
  const axiosInstance = axios.create({
-    baseURL:"http://localhost:5100/api"
+    baseURL:"https://apnidukan-backend-1.onrender.com"
  })
  axiosInstance.interceptors.request.use((config)=>{ const token = localStorage.getItem("token");
     if(token){
